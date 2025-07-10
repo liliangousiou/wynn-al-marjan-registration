50fdad6f4a4d450b4d96f71891bef3c99dff2da1
Lilian <lilgousiou@gmail.com>
2025-07-10 19:24:17 +0300

    feat(theme): add custom fonts and color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,33 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        avenir: ['"Avenir LT Std"', 'sans-serif'],
+        caslon: ['"Big Caslon"', 'serif'],
+      },
+      colors: {
+        grey: '#565759',
+        dark: '#1D1F22',
+        green: '#006F62',
+        red: '#B3261E',
+        chocolate: '#5A3A27',
+      },
       maxWidth: {
         layout: '630px',
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addBase, theme }) {
+      addBase({
+        ':root': {
+          '--color-grey': theme('colors.grey'),
+          '--color-dark': theme('colors.dark'),
+          '--color-green': theme('colors.green'),
+          '--color-red': theme('colors.red'),
+          '--color-chocolate': theme('colors.chocolate'),
+        },
+      });
+    },
+  ],
 };
