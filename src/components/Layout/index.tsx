bad6108002333535120a930ff39295e9f2c7d9ee
Lilian <lilgousiou@gmail.com>
2025-07-14 16:29:04 +0300

    feat: add automatic scroll to top on route change

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import { Footer, Header } from 'components';
+import { useScrollToTopOnRouteChange } from 'hooks/useScrollToTopOnRouteChange';
 
 type LayoutProps = {
   children: React.ReactNode;
 };
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
+  useScrollToTopOnRouteChange(true); // false for instant scroll
+
   return (
     <div className="min-h-screen flex flex-col justify-between">
       <Header />
